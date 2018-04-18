829265a83df3311e3d04b52f83679f730c768b9e
Tushka Dermendzhieva <tushka.dermendzhieva@paysafe.com>
2018-04-18 23:09:40 +0300

    Add additional design patterns real examples

---
+++ DesignPatterns/src/mediator/MediatorDemo.java
@@ -0,0 +1,14 @@
+package mediator;
+
+public class MediatorDemo {
+
+	public static void main(String[] args) {
+		User robert = new User("Robert");
+		User john = new User("John");
+
+		robert.sendMessage("Hi! John!");
+		john.sendMessage("Hello! Robert!");
+
+	}
+
+}
